20c1e91fce2c66aa5902adfac745f9f79bcad386
Andres Villarroel <andres.via@gmail.com>
2017-08-09 20:24:54 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('<a href="https://github.com/andresvia/generate-and-serve">hello world</a>');
 });
 
