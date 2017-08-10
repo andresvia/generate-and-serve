6038841fd29d4b8513d7a98c3921c5780f8af2f9
Andres Villarroel <andres.via@gmail.com>
2017-08-09 20:24:15 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hello world');
+  res.end('<a href="https://github.com/andresvia/generate-and-serve">hello world</a>');
 });
 
 server.listen(process.env.PORT || 8080, () => {});
